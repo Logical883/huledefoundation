db64f0a0419845eb562f9bbf6e7f64e0c5550ebe
Logical883 <yeboahalbert1978@gmail.com>
2025-09-28 15:17:51 +0000

    Fix import statements in App.jsx to use lowercase file names for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-import About from "./Pages/About.jsx";
-import Contact from "./Pages/Contact.jsx";
-import Editor from "./Pages/Editor.jsx";
-import Gallery from "./Pages/Gallery.jsx";
-import Home from "./Pages/Home.jsx";
-import Projects from "./Pages/Projects.jsx";
-import Support from "./Pages/Support.jsx";
-import Team from "./Pages/Team.jsx";
+import About from "./Pages/about.jsx";
+import Contact from "./Pages/contact.jsx";
+import Editor from "./Pages/editor.jsx";
+import Gallery from "./Pages/gallery.jsx";
+import Home from "./Pages/home.jsx";
+import Projects from "./Pages/projects.jsx";
+import Support from "./Pages/support.jsx";
+import Team from "./Pages/team.jsx";
 
 function App() {
   // Height of navbar (adjust if you change navbar height in CSS)
