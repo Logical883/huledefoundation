ef697173e7f21bd6c883036136ff2a5fce4ecf19
Logical883 <yeboahalbert1978@gmail.com>
2025-09-28 01:35:05 +0000

    Add responsive styles and mobile menu functionality to Navbar and Hero components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import menu_icon from "../../assets/menu-icon.png";
 
 const Navbar = () => {
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
   return (
     <nav className="container">
       <img src={logo} alt="" className="logo" />
-      <ul>
+      <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
         <li>Home</li>
         <li>About Us</li>
         <li>Projects</li>
@@ -18,6 +23,7 @@ const Navbar = () => {
           <button className="btn">Contact Us</button>
         </li>
       </ul>
+      <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
     </nav>
   );
 };
